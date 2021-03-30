28e65051770129826b19eefd58795da12ff7ec5e
Andres Ricardo Perez <riperezro@unal.edu.co>
2021-03-29 21:27:18 -0500

    initial Spring Boot setup

---
+++ ParkingManager/ParkingManager/src/test/kotlin/co/edu/unal/parkinspace/parkingManager/ParkingManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.edu.unal.parkinspace.parkingManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkingManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
