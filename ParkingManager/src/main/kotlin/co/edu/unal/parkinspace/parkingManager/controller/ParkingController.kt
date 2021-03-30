00e63f7bf6e58ef201c959b7dde097b463c7c4e0
Andres Ricardo Perez <riperezro@unal.edu.co>
2021-03-29 23:57:35 -0500

    Service + Controller template

---
+++ ParkingManager/src/main/kotlin/co/edu/unal/parkinspace/parkingManager/controller/ParkingController.kt
@@ -0,0 +1,7 @@
+package co.edu.unal.parkinspace.parkingManager.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ParkingController {
+}
---
+++ ParkingManager/src/main/kotlin/co/edu/unal/parkinspace/parkingManager/service/ParkingService.kt
@@ -0,0 +1,7 @@
+package co.edu.unal.parkinspace.parkingManager.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class ParkingService {
+}
