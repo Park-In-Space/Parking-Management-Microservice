6aa7767d761fa4929f5d57e6d6e664830409c7c2
Andres Ricardo Perez Rojas <riperezro@unal.edu.co>
2021-03-30 14:18:50 -0500

    Fixed id column name in model

--- ParkingManager/src/main/kotlin/co/edu/unal/parkinspace/parkingManager/dataAccess/model/Parking.kt
+++ ParkingManager/src/main/kotlin/co/edu/unal/parkinspace/parkingManager/dataAccess/model/Parking.kt
@@ -5,6 +5,7 @@ import javax.persistence.*
 @Entity( name = "parking" )
 data class Parking (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_parking")
     val id: Long = 0,
 
     @Column( name = "name" , nullable = false )
